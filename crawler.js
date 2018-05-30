2047996d9b2385feaebec725980d669155818ddf
Joey <jpg@neilpie.co.uk>
2018-05-30 15:02:44 +0100

    crawler

--- crawler.js
+++ crawler.js
@@ -39,11 +39,12 @@ blog_urls.forEach( function( url ) {
 
 function createCORSRequest( method, url ) {
     var xhr = new XMLHttpRequest();
+    xhr = new XDomainRequest();
+    xhr.open( method, url );
+    console.log ( xhr );
     if( "withCredentials" in xhr ) {
-        console.log( 'first' );
         xhr.open( method, url, true );
     } else if( typeof XDomainRequest != "undefined" ) {
-        console.log( 'second' );
         xhr = new XDomainRequest();
         xhr.open( method, url );
     } else {
